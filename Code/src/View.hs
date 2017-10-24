6ea26a78d3f0aa8dd7f5cc28cc6d6cc59ab6b336
Thomas Wojcik <thomas-wojcik@hotmail.com>
2017-10-24 17:03:12 +0200

    Translate & Scale
    kinda ugly coded, but it works

--- Code/src/View.hs
+++ Code/src/View.hs
@@ -13,6 +13,7 @@ drawing a list of pictures, using Pictures, means the first will be in the back.
 So, Pictures [GameMap, Boosts, Bombs, Opponents, Player] should draw it correctly.
 
 Translate all the Sprites of all objects in the [GameObject] to their GridPosition.
+Scale all the Sprites to be the correct size
 Add all the sprites above to one list
 Call Pictures on the list above
 -}
@@ -22,3 +23,12 @@ viewPure gstate = case infoToShow gstate of
   ShowNothing   -> blank
   ShowANumber n -> color green (text (show n))
   ShowAChar   c -> color green (text [c])
+
+-- Currently only works if the map is a square, collums and rows can be even or uneven
+translatePicture :: Float -> Float -> Float -> Float -> Picture -> Picture
+translatePicture x y cols size pic = translate (scale x (size / cols)) (scale (1 + cols - y) (size / cols)) pic
+       where scale a b | a > (cols / 2) = (size / cols) * (a - (cols / 2)) - (size / cols / 2)
+                       | otherwise      = -1 * (size / cols) * ((cols / 2) - a) - (size / cols / 2)
+
+scalePicture :: Float -> Float -> Picture -> Picture
+scalePicture cols size pic = scale (size / cols / (fst (snd (boundingBox pic)))) (size / cols / (snd (snd (boundingBox pic)))) pic
